4c1f55ead73970f655839887c4b4cb92f1ca2438
Hondodizzy <harveychronos@gmail.com>
2025-04-11 16:45:26 +0200

    Add files via upload

---
+++ roblox development studio website/script.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thanks for reaching out! We'll get back to you soon.");
+    this.reset();
+  });
+  
